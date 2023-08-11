894319bf07d34ff492231fbd6b19e76f1c14aeb6
Camila <>
2023-08-11 20:12:34 +0200

    you can now load the trained unet from disk and train with the sensor data

--- models/data_encoder.py
+++ models/data_encoder.py
@@ -5,14 +5,15 @@ from torch import nn
 class DataEncoder(nn.Module):
     # input data is 16x16x2
     # output data is 16x16x1 (depends on the image size and the number of channels)
-    def __init__(self, channels=(16, 32, 64), bottleneck_channels=1):
+    def __init__(self, channels=(16, 32, 64), bottleneck_channels=1,
+                 kernels=(3, 3, 3)):
         super().__init__()
         blocks = []
         channels = [2] + list(channels)
         for i in range(len(channels) - 1):
             blocks.append(
                 nn.Sequential(
-                    nn.Conv2d(channels[i], channels[i + 1], kernel_size=3, padding=1),
+                    nn.Conv2d(channels[i], channels[i + 1], kernel_size=kernels[i], padding="same"),
                     nn.BatchNorm2d(channels[i + 1]),
                     nn.ReLU(),
                 )
